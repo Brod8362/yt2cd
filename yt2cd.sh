2366678dcba9024f28a3cd4552ee928957c3eb83
Brod8362 <brod8362@gmail.com>
2021-08-27 23:12:59 -0500

    Final shape for the most part

--- yt2cd.sh
+++ yt2cd.sh
@@ -13,9 +13,8 @@ error () {
 }
 
 yes_no () {
-  echo "$1 [Y/n]"
-  read input
-  if [ $input = "Y"] | [ $input = "y" ]; then
+  read -r -p "$1 (Y/n)" input
+  if [ $input = "Y" ] || [ $input = "y" ]; then
     return 1
   fi
   return 0
@@ -42,33 +41,20 @@ if [ -d "./yt2cd_data" ]; then
   yes_no "A data folder for yt2cd already exists. Do you want to delete it?"
   if [ $? -eq 1 ]; then
     rm -r yt2cd_data
-  else
-    exit 0
   fi
 
 fi
 
-mkdir -p yt2cd_data
+mkdir -p yt2cd_data/music
 cd yt2cd_data
 #start the videos downloading
-youtube-dl --download-archive downloaded.txt -xiq --audio-format mp3 "$url" &
+youtube-dl --download-archive downloaded.txt -xiq --audio-format mp3 "$url" -o "music/%(title)s.%(ext)s" &
 info "Began video downloading"
-
-use_wavegain=0
-command -v wavegain &>/dev/null
-if [ $? -eq 0 ]; then
-  yes_no "Do you want to use wavegain to normalize volume levels?"
-  use_wavegain=$?
-else
-  info "Install wavegain to be able to normalize volume levels."
-fi
-
-
 
 cd_path="/dev/sr0"
 echo "Path to CD drive [default $cd_path]:"
 read cd_path_tmp
-if [ $cd_path_tmp = "" ]; then
+if [ ! $cd_path_tmp = "" ]; then
   cd_path=cd_path_tmp
 fi
 
@@ -77,12 +63,19 @@ if [ ! -f "$cd_path" ]; then
   exit 3
 fi
 
+info "Waiting for youtube-dl..."
 wait
 if [ $? -eq 1 ]; then
   error "youtube-dl returned with non-zero exit code $?"
   exit 2
 fi
 
-
-
-
+# TODO allow renaming the volume here instead of just YT2CD
+info "Creating CD session..."
+mkisofs -M $cd_path -C "$(cdrecord dev=$cd_path -msinfo)" -V "YT2CD" -J -r -o session.iso ./music
+info "Beginning cdrecord..."
+cdrecord -v -multi dev=$cd_path session.iso
+if [ $? -ne 0 ]; then
+  error "cdrecord exited with non-zero exit code"
+  exit 4
+fi
